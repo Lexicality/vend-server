9d9d04b3ad9ae22cd3cbb7378dc8b9828be553eb
Lex Robinson <lex@lex.me.uk>
2017-02-20 12:08:32 +0000

    a bit more hello world

--- src/github.com/lexicality/vend-server/vend-server.go
+++ src/github.com/lexicality/vend-server/vend-server.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/go-martini/martini"
+	"github.com/op/go-logging"
+)
+
+var log = logging.MustGetLogger("test")
 
 func main() {
 	fmt.Println("Hello World")
+	m := martini.Classic()
+	m.Get("/", func() string {
+		return "Hello world!"
+	})
+	m.Get("/ws", wsHandler)
+	m.RunOnAddr(":8080")
 }
---
+++ src/github.com/lexicality/vend-server/ws.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	_ = conn.WriteMessage(websocket.TextMessage, []byte("hi!"))
+
+	conn.Close()
+}
