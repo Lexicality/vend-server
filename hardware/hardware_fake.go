707148d0789bc27623ae1878f65a1708edcaeaa5
Lex Robinson <lexi@lexi.org.uk>
2017-03-12 12:03:07 +0000

    Vending takes a while

--- hardware/hardware_fake.go
+++ hardware/hardware_fake.go
@@ -4,6 +4,7 @@ package hardware
 
 import (
 	"context"
+	"time"
 
 	"github.com/lexicality/vending/vend"
 	"github.com/op/go-logging"
@@ -26,7 +27,14 @@ func (hw *hardware) Teardown() error {
 
 func (hw *hardware) Vend(ctx context.Context, location uint8) vend.Result {
 	if hw.log != nil {
-		hw.log.Infof("=== I AM VENDING ITEM #%d! ===", location)
+		hw.log.Infof("Starting simulated vend of item %d", location)
 	}
+
+	<-time.After(time.Second * 15)
+
+	if hw.log != nil {
+		hw.log.Infof("Completed simulated vend of item %d", location)
+	}
+
 	return vend.ResultSuccess
 }
