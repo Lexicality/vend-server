e078da0fede761d8cbdb22ce8700ec07a086ab8a
Lex Robinson <lexi@lexi.org.uk>
2017-03-14 14:21:36 +0000

    Super crude but working deferred vending
    & it works with graceful shutdown & everythingggg

--- web/money.go
+++ web/money.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
@@ -10,9 +11,38 @@ import (
 	"github.com/stripe/stripe-go/charge"
 
 	"github.com/lexicality/vending/backend"
+	"github.com/lexicality/vending/hardware"
+	"github.com/lexicality/vending/vend"
 )
 
-func handleBuy(r render.Render, stock *backend.Stock, req *http.Request, log *logging.Logger) {
+// VendSession represents an attempt to vend, ideally so you can go find it again
+type vendSession struct {
+	ID    string
+	State vend.Result
+}
+
+// StartVending starts a blocking attempt to vend
+func (s *vendSession) startVending(
+	ctx context.Context,
+	hw *hardware.Machine,
+	stock *backend.Stock,
+	item *backend.StockItem,
+) {
+	stock.ReserveItem(ctx, item.ID)
+
+	res := hw.Vend(ctx, item.Location)
+	s.State = res
+
+	stock.UpdateItem(context.TODO(), item.ID, res)
+}
+
+func handleBuy(
+	r render.Render,
+	stock *backend.Stock,
+	hw *hardware.Machine,
+	req *http.Request,
+	log *logging.Logger,
+) {
 	ctx := req.Context()
 
 	// TODO: Parse & validate
@@ -46,6 +76,18 @@ func handleBuy(r render.Render, stock *backend.Stock, req *http.Request, log *lo
 		r.Text(500, err.Error())
 		return
 	}
+
+	// https://i.imgur.com/mibus.jpg
+	vs := &vendSession{
+		ID:    charge.ID,
+		State: vend.NoResult,
+	}
+	go vs.startVending(
+		ctx,
+		hw,
+		stock,
+		item,
+	)
 
 	r.Text(200, fmt.Sprintf("%+v", charge))
 }
