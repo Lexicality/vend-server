d6009b49b6e25fa6328638329ebb21412eb57388
Lex Robinson <lexi@lexi.org.uk>
2017-03-18 00:10:03 +0000

    Remove old result values

--- vend/results.go
+++ vend/results.go
@@ -10,10 +10,6 @@ const (
 	ResultSuccess
 	// ResultEmpty is when you vend something that's not there
 	ResultEmpty
-	// ResultBusy is when the vending machine is already vending
-	ResultBusy
-	// ResultOffline is when the vending machine isn't talking to us right now
-	ResultOffline
 	// ResultJammed is when the vending machine physically can't vend
 	ResultJammed
 	// ResultHardwareFailure is when the vending machine is on fire
@@ -30,8 +26,6 @@ const (
 var AllResults = map[string]Result{
 	"ResultSuccess":         ResultSuccess,
 	"ResultEmpty":           ResultEmpty,
-	"ResultBusy":            ResultBusy,
-	"ResultOffline":         ResultOffline,
 	"ResultJammed":          ResultJammed,
 	"ResultHardwareFailure": ResultHardwareFailure,
 	"ResultUnknownFailure":  ResultUnknownFailure,
